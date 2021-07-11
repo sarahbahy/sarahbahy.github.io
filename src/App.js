4cca43b9585a81fd9a0d5f9f33d99a65c7f9e23e
sarahbahi <sarahbahyy@gmail.com>
2021-07-11 02:05:57 +0200

    create app

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h2>hellooo</h2>
+        <h2>hellooo world</h2>
       </header>
     </div>
   );
